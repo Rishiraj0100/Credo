edbba630ef785ff761377d520e9f55481b0da517
TierGamerpy <adityabhushan934@gmail.com>
2021-06-29 10:25:30 +0530

    Create functions.py

---
+++ src/models/functions.py
@@ -0,0 +1,24 @@
+import typing
+from pypika.terms import Function
+from tortoise.expressions import F
+from enum import Enum
+
+
+__all__ = (
+    "ArrayAppend",
+    "ArrayRemove",
+)
+
+
+class ArrayAppend(Function):
+    def __init__(self, field: str, value: typing.Any) -> None:
+        if isinstance(value, Enum):
+            value = value.value
+
+        super().__init__("ARRAY_APPEND", F(field), str(value))
+
+
+class ArrayRemove(Function):
+    def __init__(self, field: str, value: typing.Any) -> None:
+        if isinstance(value, Enum):
+            value = value.value
