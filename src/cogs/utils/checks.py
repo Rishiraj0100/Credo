e138e079ec4cbb740ee686c6fb085c8571b7127c
Aditya Bhushan <adityabhushan934@gmail.com>
2021-07-17 05:34:55 +0100

    Update checks.py

--- src/cogs/utils/checks.py
+++ src/cogs/utils/checks.py
@@ -1,31 +1,51 @@
 from discord.ext import commands
-from . import emote
+from . import emote,context,expectations
 
-def is_bot_setuped():
-    async def predicate(ctx):
-        if ctx.guild is None:
-            return False
+__all__ = (
+    "SMNotUsable",
+    "can_use_sm"
+)
 
-        data = await ctx.db.fetchval('SELECT is_bot_setuped FROM server_configs WHERE guild_id = $1',ctx.guild.id)
-        if data == False:
-            await ctx.send(f'{emote.error} | This Server Dose Not Have Bot Setuped Here Use `*setup`')
-            False
-            return
+class SMNotUsable(expectations.Credoerror):
+    def __init__(self):
+        super().__init__(f"You need either the `credo-smanager` role or `manage_guild` permissions to use scrims manager.")
 
-        return True
+# def is_bot_setuped():
+#     async def predicate(ctx):
+#         if ctx.guild is None:
+#             return False
+
+#         data = await ctx.db.fetchval('SELECT is_bot_setuped FROM server_configs WHERE guild_id = $1',ctx.guild.id)
+#         if data == False:
+#             await ctx.send(f'{emote.error} | This Server Dose Not Have Bot Setuped Here Use `*setup`')
+#             False
+#             return
+
+#         return True
+#     return commands.check(predicate)
+# def is_smanager_setuped():
+#     async def predicate(ctx):
+#         if ctx.guild is None:
+#             return False
+
+#         data = await ctx.db.fetchval('SELECT scrims_manager FROM server_configs WHERE guild_id = $1',ctx.guild.id)
+#         if data == False:
+#             await ctx.send(f'{emote.error} | This Server Dose Not Have Smanager Setuped Here Use `*smanager setup`')
+#             False
+#             return
+
+#         return True
+#     return commands.check(predicate)
+
+def can_use_sm():
+    """
+    Returns True if the user has manage roles or credo-smanager role in the server.
+    """
+
+    async def predicate(ctx: context.Context):
+        if ctx.author.guild_permissions.manage_guild or "credo-smanager" in (role.name.lower() for role in ctx.author.roles):
+            return True
+        else:
+            raise SMNotUsable()
+
     return commands.check(predicate)
-def is_smanager_setuped():
-    async def predicate(ctx):
-        if ctx.guild is None:
-            return False
-
-        data = await ctx.db.fetchval('SELECT scrims_manager FROM server_configs WHERE guild_id = $1',ctx.guild.id)
-        if data == False:
-            await ctx.send(f'{emote.error} | This Server Dose Not Have Smanager Setuped Here Use `*smanager setup`')
-            False
-            return
-
-        return True
-    return commands.check(predicate)
-
-#todo make premium checks
